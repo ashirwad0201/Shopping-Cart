a5aba793d7483b9200eadd89dd0a8261caa2871f
ashirwad0201 <ashirwad0201@gmail.com>
2023-12-23 00:09:44 +0530

    controllers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const errorController=require('./controllers/error')
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -17,8 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
